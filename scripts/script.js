60d9ea5593ffbabd78cd7eb7ca7675401a040021
FN2211 <91390444+FN2211@users.noreply.github.com>
2024-05-02 17:16:35 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,25 @@
-let homeBtn = document.getElementById("home")
-homeBtn.addEventListener("click", function () {
-    let audio = new Audio("../assets/töröö.mp3");
-    audio.play()
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    const Email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    if(auth(Email, password)){
+        alert("correct")
+        Email.value = ""
+        password.value = ""
+    }else {
+        alert("wrong")
+    }
 })
+
+
+function auth(email,password){
+    if(email === "admin@adventurestudio.com" && password === "admin"){
+        return true
+    } else {
+        return false
+    }
+}
