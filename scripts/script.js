9dfa93149c5f0702d709b87678e91e10801508b2
FN2211 <91390444+FN2211@users.noreply.github.com>
2024-05-03 22:05:11 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
+gsap.fromTo(
+    '.loading-page',
+    {opacity: 1 },
+    {
+        opacity:0,
+        duration: 1.5,
+        delay: 6
+    }
+);
 
+gsap.fromTo(
+    'header',
+    {opacity: 0 },
+    {
+        opacity:1,
+        duration: 1.5,
+        delay: 6
+    }
+);
+
+gsap.fromTo(
+    '.loading-logo-name',
+    { opacity: 0,
+      y:50
+
+    },{
+        y: 0,
+        opacity:1,
+        duration: 2,
+        delay: 0.5
+    }
+);
